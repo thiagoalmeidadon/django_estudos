57cc8ba71224da0cf7fbe720ba06b07891f8d8d4
thiagoalmeidadon <thiagodons@gmail.com>
2021-10-07 13:59:55 -0300

    implementando forms, bootstrap4, post

--- inter/core/views.py
+++ inter/core/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .forms import ContatoForm
+from django.contrib import messages
 
 # Create your views here.
 
@@ -6,7 +8,25 @@ def index(request):
     return render(request, 'index.html')
 
 def contato(request):
-    return render(request, 'contato.html')
+    form = ContatoForm(request.POST or None)
+
+    if str(request.method) == 'POST':
+        if form.is_valid():
+            nome = form.cleaned_data['nome']
+            email = form.cleaned_data['email']
+            assunto = form.cleaned_data['assunto']
+            mensagem = form.cleaned_data['mensagem']
+            print(f'nome: {nome}, email: {email}, mesagem: {mensagem}')
+
+            messages.success(request, 'Mensagem enviada com sucesso!')
+            form = ContatoForm()
+        else:
+            messages.error(request, 'Erro ao enviar sua mensagem!')
+
+    context = {
+        'form' : form
+    }
+    return render(request, 'contato.html', context)
 
 def produto(request):
     return render(request, 'produto.html')
