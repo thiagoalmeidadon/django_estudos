733b24f92f02fb8c1ba33213d5aa8e40062f799c
thiagoalmeidadon <thiagodons@gmail.com>
2021-10-07 15:13:40 -0300

    definindo models produto

--- inter/core/models.py
+++ inter/core/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.db.models.base import Model
+from stdimage.models import StdImageField
 
-# Create your models here.
+# Signals
+from django.db.models import signals
+from django.template.defaultfilters import default, slugify
+
+class Base(models.Model):
+    criado = models.DateField('Data de criação', auto_now_add=True)
+    modificado = models.DateField('Data de Atualização', auto_now=True)
+    ativo = models.BooleanField('Ativo?', default=True)
+
+    class Meta:
+        abstract = True
+
+class Produto(Base):
+    nome = models.CharField('Nome', max_length=100)
+    preco = models.DecimalField('Preço', max_digits=8, decimal_places=2)
+    estoque = models.IntegerField('Estoque')
+    imagem = StdImageField('Imagem', upload_to='produtos', variations={'thumb':(124,124)})
+    slug = models.SlugField('Slug', max_length=100, blank=True, editable=False)
+
+    def __str__(self):
+        return self.nome
+
+
+#Signals função fora da classe 
+def produto_pre_save(signal, instance, sender, **kwargs):
+    instance.slug = slugify(instance.nome)
+
+signals.pre_save.connect(produto_pre_save, sender=Produto)
