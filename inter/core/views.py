1017fead9298e7c00f6098f65095f5edffcc88e9
thiagoalmeidadon <thiagodons@gmail.com>
2021-10-07 17:58:14 -0300

    listando dados na index

--- inter/core/views.py
+++ inter/core/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
-from .forms import ContatoForm, ProdutoModelForm
 from django.contrib import messages
+
+from .forms import ContatoForm, ProdutoModelForm
+from .models import Produto
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    context = {
+        'produtos' : Produto.objects.all()
+    }
+    return render(request, 'index.html', context)
 
 def contato(request):
     form = ContatoForm(request.POST or None)
